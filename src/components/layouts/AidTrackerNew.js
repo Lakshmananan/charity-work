3a4178baceb515426fe28999e9f3ddbdddf433a8
lakshmananan <lakshman@datasphere.ca>
2020-01-10 09:56:36 -0500

    Finished boxes for aid tracker.

--- src/components/layouts/AidTrackerNew.js
+++ src/components/layouts/AidTrackerNew.js
@@ -10,12 +10,12 @@ const AidTrackerNew = () => {
               className='card blue-grey darken-1'
               style={{ height: '250px' }}
             >
-              <div className='card-content white-textr'>
+              <div className='card-content white-text'>
                 <br />
                 <br />
-                <br />
-                <span className='card-title'>Card Title</span>
-                <p>Content here</p>
+                <i class='medium material-icons'>build</i>
+                <span className='card-title'>Wells Built</span>
+                <span className='card-title'>40</span>
               </div>
             </div>{' '}
           </div>
@@ -24,12 +24,12 @@ const AidTrackerNew = () => {
               className='card blue-grey darken-1'
               style={{ height: '250px' }}
             >
-              <div className='card-content white-textr'>
+              <div className='card-content white-text'>
                 <br />
                 <br />
-                <br />
-                <span className='card-title'>Card Title</span>
-                <p>Content here</p>
+                <i class='medium material-icons'>school</i>
+                <span className='card-title'>Schools Created</span>
+                <span className='card-title'>70</span>
               </div>
             </div>
           </div>
@@ -38,12 +38,12 @@ const AidTrackerNew = () => {
               className='card blue-grey darken-1'
               style={{ height: '250px' }}
             >
-              <div className='card-content white-textr'>
+              <div className='card-content white-text'>
                 <br />
                 <br />
-                <br />
-                <span className='card-title'>Card Title</span>
-                <p>Content here</p>
+                <i class='medium material-icons'>people</i>
+                <span className='card-title'>Students Enrolled</span>
+                <span className='card-title'>500</span>
               </div>
             </div>
           </div>
@@ -52,18 +52,18 @@ const AidTrackerNew = () => {
               className='card blue-grey darken-1'
               style={{ height: '250px' }}
             >
-              <div className='card-content white-textr'>
+              <div className='card-content white-text'>
                 <br />
                 <br />
-                <br />
-                <span className='card-title'>Card Title</span>
-                <p>Content here</p>
+                <i class='medium material-icons'>fingerprint</i>
+                <span className='card-title'>People Helped</span>
+                <span className='card-title'>1000</span>
               </div>
             </div>
           </div>
         </div>
         <div className='row'>
-          <p>Updated 10AM Today | Since Aug 2017</p>
+          <h6>Updated 10AM Today | Since Aug 2017</h6>
         </div>
       </div>
     </div>
