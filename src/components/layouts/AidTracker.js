b4d2de3a9397666dc6b620d91a95b16a572daf5d
lakshmananan <lakshman@datasphere.ca>
2020-01-10 11:00:08 -0500

    Made AidTrackerItem component.

--- src/components/layouts/AidTracker.js
+++ src/components/layouts/AidTracker.js
@@ -1,68 +1,18 @@
 import React from 'react';
-
+import AidTrackerItem from './AidTrackerItem';
 const AidTracker = () => {
   return (
-    <div>
-      <div className='container center'>
-        <h1>What We Did</h1>
-        <br />
-        <div className='row'>
-          <div className='col m3 s12'>
-            <div
-              className='card blue-grey darken-1 z-depth-4'
-              style={{ height: '250px' }}
-            >
-              <div className='card-content white-text'>
-                <br />
-                <i className='medium material-icons'>build</i>
-                <span className='card-title'>Wells Constructed</span>
-                <span className='card-title'>40</span>
-              </div>
-            </div>{' '}
-          </div>
-          <div className='col m3 s12'>
-            <div
-              className='card blue-grey darken-1 z-depth-4'
-              style={{ height: '250px' }}
-            >
-              <div className='card-content white-text'>
-                <br />
-                <i className='medium material-icons'>school</i>
-                <span className='card-title'>Schools Created</span>
-                <span className='card-title'>70</span>
-              </div>
-            </div>
-          </div>
-          <div className='col m3 s12'>
-            <div
-              className='card blue-grey darken-1 z-depth-4'
-              style={{ height: '250px' }}
-            >
-              <div className='card-content white-text'>
-                <br />
-                <i className='medium material-icons'>people</i>
-                <span className='card-title'>Students Enrolled</span>
-                <span className='card-title'>500</span>
-              </div>
-            </div>
-          </div>
-          <div className='col m3 s12'>
-            <div
-              className='card blue-grey darken-1 z-depth-4'
-              style={{ height: '250px' }}
-            >
-              <div className='card-content white-text'>
-                <br />
-                <i className='medium material-icons'>fingerprint</i>
-                <span className='card-title'>People Helped</span>
-                <span className='card-title'>1000</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='row'>
-          <h6>Updated 10AM Today | Since Aug 2017</h6>
-        </div>
+    <div className='container center'>
+      <h1>What We Did</h1>
+      <br />
+      <div className='row'>
+        <AidTrackerItem icon='build' title='Wells Constructed' count='40' />
+        <AidTrackerItem icon='school' title='Schools Created' count='70' />
+        <AidTrackerItem icon='people' title='Students Enrolled' count='500' />
+        <AidTrackerItem icon='fingerprint' title='People Helped' count='1000' />
+      </div>
+      <div className='row'>
+        <h6>Updated 10AM Today | Since Aug 2017</h6>
       </div>
     </div>
   );
