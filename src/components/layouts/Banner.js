a1639f28d4bb719688dcf30c5dce04c41ddfd5f6
lakshmananan <lakshman@datasphere.ca>
2020-01-10 12:55:46 -0500

    Tried fixing banner.

--- src/components/layouts/Banner.js
+++ src/components/layouts/Banner.js
@@ -11,32 +11,22 @@ const Banner = () => {
           <li>
             <img src={backgroundImg} alt='' />
             <div className='caption center-align'>
-              <h3 className='pink-text text-lighten-2'>
-                This is our big Tagline!
-              </h3>
-              <h5 className='pink-text text-lighten-2'>
-                Here's our small slogan.
-              </h5>
+              <h1 className='pink-text'>The Ilahi Foundation</h1>
+              <h3 className='pink-text '>A good cause </h3>
             </div>
           </li>
           <li>
             <img src={backgroundImg2} alt='' />
             <div className='caption left-align'>
-              <h3 className='pink-text text-lighten-2'>Left Aligned Caption</h3>
-              <h5 className='pink-text text-lighten-2'>
-                Here's our small slogan.
-              </h5>
+              <h1 className='pink-text '>The Ilahi Foundation</h1>
+              <h3 className='pink-text '>A good cause </h3>
             </div>
           </li>
           <li>
             <img src={backgroundImg3} alt='' />
             <div className='caption right-align'>
-              <h3 className='pink-text text-lighten-2'>
-                Right Aligned Caption
-              </h3>
-              <h5 className='pink-text text-lighten-2'>
-                Here's our small slogan.
-              </h5>
+              <h1 className='pink-text '>The Ilahi Foundation</h1>
+              <h3 className='pink-text '>A good cause</h3>
             </div>
           </li>
         </ul>
